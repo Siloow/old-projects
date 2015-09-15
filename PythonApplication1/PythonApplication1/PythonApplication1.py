28613f1a14fb971aa5f0de0af9baaf4d354ebd48
Siloow <sil.de.graaf96@gmail.com>
2015-09-15 15:52:19 +0200

    Added functionality

--- PythonApplication1/PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -1,1 +1,9 @@
-print("Hello world!")
+﻿value_input = input("Fill in the size of the cube: ")
+value_output = 1
+string_out = "x"
+string_space = " "
+print (value_input * "_ ")
+while value_input > 0:
+    print("|" + string_out * value_input + "/" + value_output * string_space + "|")
+    value_input = value_input - 1
+    value_output = value_output + 1
