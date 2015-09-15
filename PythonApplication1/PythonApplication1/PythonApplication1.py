f7f82ce92efcba7bec9e5092076cc80703f20737
Siloow <sil.de.graaf96@gmail.com>
2015-09-15 15:59:05 +0200

    Added last line

--- PythonApplication1/PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -2,8 +2,10 @@
 value_output = 1
 string_out = "x"
 string_space = " "
-print (value_input * "_ ")
+print (value_input * "_")
 while value_input > 0:
     print("|" + string_out * value_input + "/" + value_output * string_space + "|")
     value_input = value_input - 1
     value_output = value_output + 1
+    
+print (value_output * "_")
