65c38b45eaaefe6a805a3e996872859bc280bf3d
Siloow <sil.de.graaf96@gmail.com>
2015-09-23 09:10:28 +0200

    added assignment4.1

---
+++ Assignment4Exercise1/Assignment4Exercise1/Assignment4Exercise1.py
@@ -0,0 +1,12 @@
+temparature = input("What temeparature do you want to be converted to celsius?\n")
+
+def convert(temparature):
+    fahrenfeit = (temparature - 32) / 1.8000
+    print fahrenfeit
+    return
+
+
+convert(temparature)
+
+
+
