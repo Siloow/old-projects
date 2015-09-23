484b2995b0956f69b836bb1b6759938075f760d7
Siloow <sil.de.graaf96@gmail.com>
2015-09-23 09:52:16 +0200

    Fixed assingment4excercise1

--- Assignment4Exercise1/Assignment4Exercise1/Assignment4Exercise1.py
+++ Assignment4Exercise1/Assignment4Exercise1/Assignment4Exercise1.py
@@ -1,8 +1,14 @@
-﻿temparature = input("What temeparature do you want to be converted to celsius?\n")
+﻿temparature = input("What temeparature from fahrenheit do you want to be converted to celsius?\n")
 
 def convert(temparature):
-    fahrenfeit = (temparature - 32) / 1.800
-    return float(format(fahrenfeit, '.2f'))
+    if temparature < -273.15:
+        print "You cannot go lower than 273.15 fahrenheit, please try again"
+        temparature = input("What temeparature from fahrenheit do you want to be converted to celsius?\n")
+        fahrenheit = (temparature - 32) / 1.8000
+        return float(format(fahrenheit, '.2f'))
+    else:
+        fahrenheit = (temparature - 32) / 1.8000
+        return float(format(fahrenheit, '.2f'))
 
 print convert(temparature)
 
