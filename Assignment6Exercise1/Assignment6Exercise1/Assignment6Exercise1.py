b8a5a17092cebe71cfa4bff037fe69255c17620e
Siloow <sil.de.graaf96@gmail.com>
2015-10-05 15:37:45 +0200

    Added assignment 6

---
+++ Assignment6Exercise1/Assignment6Exercise1/Assignment6Exercise1.py
@@ -0,0 +1,20 @@
+﻿# full square
+a = int(raw_input("How big do you want the full square to be?\n"))
+
+m, n = a, a
+
+for i in range (m):
+    for j in range (n):
+        print '*',
+    print 
+
+
+# hollow square
+a = int(raw_input("How big do you want the hollow square to be?\n"))
+
+m, n = a, a
+
+for i in range (m):
+    for j in range(n):
+        print '*' if i in [0, n-1] or j in [0, m-1] else ' ',
+    print ' '
