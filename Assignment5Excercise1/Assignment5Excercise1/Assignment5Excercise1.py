8b092821b5779d46a88c492172207bfa69ed2531
Siloow <sil.de.graaf96@gmail.com>
2015-10-05 20:43:16 +0200

    Edited assignements 5.1 and Turtle

--- Assignment5Excercise1/Assignment5Excercise1/Assignment5Excercise1.py
+++ Assignment5Excercise1/Assignment5Excercise1/Assignment5Excercise1.py
@@ -1,3 +1,6 @@
-user_input = raw_input("Type in a sentence you want reversed")
+﻿user_input = raw_input("Type in a sentence you want reversed")
+message = ""
 
-print user_input[::-1]
+for i in user_input:
+    message = i + message
+print (message)
