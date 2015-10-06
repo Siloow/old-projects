3f0c98dd6bdca7ea5bfa366d22db159ba8c827ce
Siloow <sil.de.graaf96@gmail.com>
2015-10-06 23:33:57 +0200

    Added last shapes

--- Assignment6/Assignment6.1/Assignment6.1.py
+++ Assignment6/Assignment6.1/Assignment6.1.py
@@ -3,8 +3,8 @@ a = int(raw_input("How big do you want the full square to be?\n"))
 
 m, n = a, a
 
-for i in range (m):
-    for j in range (n):
+for i in range (m): # in range of (1, m)
+    for j in range (n): # in range of (1, n)
         print '*',
     print 
 
@@ -16,15 +16,27 @@ m, n = a, a
 
 for i in range (m):
     for j in range(n):
-        print '*' if i in [0, n-1] or j in [0, m-1] else ' ',
+        print '*' if i in [0, n-1] or j in [0, m-1] else ' ', # only print * if i is in array [0, n-1] so that it only prints the outline
     print
 
 # triangle
-a = int(raw_input("How big do you want the full square to be?\n"))
+a = int(raw_input("How big do you want the triangle to be?\n"))
 
 m, n = a, a
 
 for i in range (m):
-    for j in range (i + 1):
+    for j in range (i + 1): # 1 gets added to i each iteration so that it print an extra *
+        print '*',
+    print 
+
+# pyramid
+a = int(raw_input("How big do you want the pyramid to be?\n"))
+
+n = a
+
+for i in range(1,n+1):
+    for j in range(1,n-i+1): # in range of (1, (4, 3, 2, 1))
+        print ' ',
+    for k in range(1,2*i): # in range of (1, (2, 4, 6, 8, 10))
         print '*',
     print 
