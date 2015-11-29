19f11272b2f6c0c39e0c85bcc614a8509aa7b3e5
Sil de Graaf <sil_de_graaf@hotmail.com>
2015-11-29 20:01:18 +0100

    Added pygame

---
+++ Pratice/Pratice/Pygame.py
@@ -0,0 +1,14 @@
+﻿import pygame
+
+class Car():
+    def __init__(self, image, height, speed):
+        self.Speed = speed
+        self.Image = image
+        self.Position = image.get_rect().move(0, height)
+    def move(self):
+        self.Position = self.Position.move(0, self.Speed)
+        if self.Position.right > 600:
+            self.Position.left = 0
+
+screen = pygame.display.set_mode((640, 480))
+
