f368ff2c39687e4da67e8abbab9fe71e3316eb77
Siloow <sil.de.graaf96@gmail.com>
2016-04-08 12:56:31 +0200

    Added basic Bing Map

---
+++ BingMapTest/BingMapTest/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Geolocation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace BingMapTest
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            MyMap.Center = new Geopoint(
+                new BasicGeoposition()
+                {
+                    Latitude = 52.181427,
+                    Longitude = 5.399780
+                });
+            MyMap.ZoomLevel = 16;
+            base.OnNavigatedTo(e);
+        }
+    }
+}
