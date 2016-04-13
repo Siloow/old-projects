4cd103123ee377bbbdb70f2025515d2dc231fe02
Tim <0885872@hr.nl>
2016-04-13 12:31:34 +0200

    Test commit two

--- MapApp/Map/MainPage.xaml.cs
+++ MapApp/Map/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -13,6 +14,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Map;
+using System.Data;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -23,18 +25,69 @@ namespace Map
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        Boolean postcodeChanged = false;
+        string postcode;
+        string postcodeComplete;
+        string query = "SELECT * FROM dsldata WHERE postcode LIKE ";
         public MainPage()
         {
             this.InitializeComponent();
         }
 
-        private void button_Click(object sender, RoutedEventArgs e)
-        {
-            
-        }
+
 
         private void textBlock_SelectionChanged(object sender, RoutedEventArgs e)
         {
-            
+
         }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {   
+            //Standaardtest is veranderd, dus variabele naar "TRUE"
+            postcodeChanged = true;
+
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {   
+            //Als de gebruiker de standaardtest in het tekstveld veranderd heeft.
+            if (postcodeChanged == true)
+            {
+                //postcode ophalen uit tekstveld
+                postcode = textBox.Text;
+                //Controle of postcode aan format voldoet
+                if (postcode.Length > 4 || postcode.Length == 0)
+                {
+                    textBlock.Text = "U heeft een ongeldige postcode ingevoerd, probeer aub opnieuw.";
+                }
+                //postcode versturen met query
+                else
+                {
+                    query = query + postcode;
+                    //Database code.....
+                    
+                }
+
+
+            }
+            //Als de gebruiker op de knop drukt zonder de standaardtekst te hebben veranderd.
+            if (postcodeChanged == false)
+            {
+                textBlock.Text = "U heeft geen postcode ingevoerd, probeer aub opnieuw";
+            }
+
+            //string queryComplete = query + post;
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
 
